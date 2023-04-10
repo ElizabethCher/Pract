b1f8cb6dacd217b09e04554ab897f6db7ddeaeed
Алексей Оверченко <117774706+AlexeyOverchenko1@users.noreply.github.com>
2023-04-10 14:36:16 +0300

    Я заменил printf на cout
    Убрал ненужные элементы

--- main.c
+++ main.c
@@ -4,12 +4,10 @@
 int main(int argc, char **argv)
 {
     char name[255];
-
     cout<<"Enter your name: ";
     fgets(name, 255, stdin);
     cout << "length = " << strlen(name); /* debug line */
     name[strlen(name)-1] = '\0'; /* remove the newline at the end */
-
     cout<<"Hello, "<< name;
     return 0;
 }
