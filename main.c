41271a2980abeb9cdb767109dc96a57508b4ec4d
Алексей Оверченко <117774706+AlexeyOverchenko1@users.noreply.github.com>
2023-04-10 14:24:33 +0300

    Я заменил printf на cout
    Printf это устаревший метод! Cout намного лучше. (имхо)

--- main.c
+++ main.c
@@ -5,11 +5,11 @@ int main(int argc, char **argv)
 {
     char name[255];
 
-    printf("Enter your name: ");
+    cout<<"Enter your name: ";
     fgets(name, 255, stdin);
-    printf("length = %d\n", strlen(name)); /* debug line */
+    cout << "length = " << strlen(name); /* debug line */
     name[strlen(name)-1] = '\0'; /* remove the newline at the end */
 
-    printf("Hello %s!\n", name);
+    cout<<"Hello, "<< name;
     return 0;
 }
